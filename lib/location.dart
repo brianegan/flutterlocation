4813d4d7b7b78b270ee06383b4e5ded21e2a9650
Brian Egan <brian@brianegan.com>
2018-03-07 13:47:14 +0100

    Remove example. Throws lots of errors. Replacement in the works.

--- lib/location.dart
+++ lib/location.dart
@@ -2,20 +2,48 @@ import 'dart:async';
 
 import 'package:flutter/services.dart';
 
-class Location {
+class LocationPlugin {
   static const MethodChannel _channel = const MethodChannel('lyokone/location');
-  static const EventChannel _stream = const EventChannel('lyokone/locationstream');
+  static const EventChannel _stream =
+      const EventChannel('lyokone/locationstream');
 
-  Stream<Map<String,double>> _onLocationChanged;
+  Stream<Map<String, double>> _onLocationChanged;
 
-  Future<Map<String,double>> get getLocation =>
-      _channel.invokeMethod('getLocation');
+  Future<Location> get getLocation => _channel
+      .invokeMethod('getLocation')
+      .then((result) => new Location.fromResult(result));
 
-  Stream<Map<String,double>> get onLocationChanged {
+  Stream<Location> get onLocationChanged {
     if (_onLocationChanged == null) {
-      _onLocationChanged =
-          _stream.receiveBroadcastStream();
+      _onLocationChanged = _stream.receiveBroadcastStream();
     }
-    return _onLocationChanged;
+    return _onLocationChanged.map((result) => new Location.fromResult(result));
   }
+}
+
+class Location {
+  final double latitude;
+  final double longitude;
+
+  Location(this.latitude, this.longitude);
+
+  factory Location.fromResult(Map<String, double> result) {
+    return new Location(result['latitude'], result['longitude']);
+  }
+
+  @override
+  String toString() {
+    return 'LocationData{latitude: $latitude, longitude: $longitude}';
+  }
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is Location &&
+          runtimeType == other.runtimeType &&
+          latitude == other.latitude &&
+          longitude == other.longitude;
+
+  @override
+  int get hashCode => latitude.hashCode ^ longitude.hashCode;
 }
